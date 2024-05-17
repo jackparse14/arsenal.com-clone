bfb649f166450b909d98b39ccc0f717c5de9d1a9
Jack Parsons <jackparse14@hotmail.co.uk>
2024-05-17 19:15:00 +0100

    Server hosts webpage

--- app.ts
+++ app.ts
@@ -8,15 +8,22 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const __dirname = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const staticHandler = express.static(path.join(__dirname, "public"));
 const bodyParser = express.urlencoded();
 
 //  Middleware
 app.use(staticHandler);
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hell0");
+  res.sendFile(path.join(__dirname, "public", "html", "index.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).send("<h1>Not Found</h1>");
 });
 
 app.listen(port, () => {
